617f9f19603ee272cbf098e52f38653033338f70
omid <itmanager.abbasi@gmail.com>
2023-12-20 14:53:09 +0330

    Commit Domain

---
+++ TopicCleanArchitecture/TopicCleanArchitecture.Domain/Topic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopicCleanArchitecture.Domain.Common;
+
+namespace TopicCleanArchitecture.Domain
+{
+    public class Topic:BaseEntity
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; }= string.Empty;
+        public string RequestingEmployeeId { get; set; } = string.Empty;
+        public Category? Category { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
