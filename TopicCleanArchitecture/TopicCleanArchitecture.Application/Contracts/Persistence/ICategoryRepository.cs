3ca0ac8ccf75502e8b99bbaac40aef19519bc90d
omid <itmanager.abbasi@gmail.com>
2023-12-20 15:32:48 +0330

    Commit First Section of Application

---
+++ TopicCleanArchitecture/TopicCleanArchitecture.Application/Contracts/Persistence/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using TopicCleanArchitecture.Domain;
+
+namespace TopicCleanArchitecture.Application.Contracts.Persistence
+{
+    public interface ICategoryRepository<T>:IGenericRepository<Category>
+    {
+        Task<bool> IsLeaveTypeUnique(string name);
+    }
+}
---
+++ TopicCleanArchitecture/TopicCleanArchitecture.Application/Contracts/Persistence/ITopicRepository.cs
@@ -0,0 +1,9 @@
+﻿using TopicCleanArchitecture.Domain;
+
+namespace TopicCleanArchitecture.Application.Contracts.Persistence
+{
+    public interface ITopicRepository<T>:IGenericRepository<Topic>
+    {
+        
+    }
+}
