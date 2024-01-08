3068ea3c114222a90880799b014e152dbe38c131
omid <itmanager.abbasi@gmail.com>
2024-01-08 16:41:27 +0330

    commit unitTest

---
+++ TopicCleanArchitecture/TopicCleanArchitecture.Application.UnitTests/Features/Categories/Queries/GetCategoryListQueryHandlerTests.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopicCleanArchitecture.Application.Contracts.Logging;
+using TopicCleanArchitecture.Application.Contracts.Persistence;
+using TopicCleanArchitecture.Application.Features.Category.Queries.GetAllCategories;
+using TopicCleanArchitecture.Application.MappingProfiles;
+using TopicCleanArchitecture.Application.UnitTests.Mocks;
+
+namespace TopicCleanArchitecture.Application.UnitTests.Features.Categories.Queries
+{
+    public class GetCategoryListQueryHandlerTests
+    {
+        private readonly Mock<ICategoryRepository> _mockRepo;
+        private IMapper _mapper;
+        private Mock<IAppLogger<GetCategoriesQueryHandler>> _mockAppLogger;
+
+        public GetCategoryListQueryHandlerTests()
+        {
+            _mockRepo = MockCategoryRepository.GetCategoryMockCategoryRepository();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<CategoryProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+            _mockAppLogger = new Mock<IAppLogger<GetCategoriesQueryHandler>>();
+        }
+
+        [Fact]
+        public async Task GetCategoryListTest()
+        {
+            var handler = new GetCategoriesQueryHandler(_mapper, _mockRepo.Object, _mockAppLogger.Object);
+
+            var result = await handler.Handle(new GetCategoriesQuery(),CancellationToken.None);
+
+            result.ShouldBeOfType<List<CategoryDto>>();
+            result.Count.ShouldBe(3);
+            if (result.Count > 0)
+                result.Where(x => x.Id == 2).Single().Name = "test";
+            result.FirstOrDefault(x=>x.Id==2).ShouldBeEquivalentTo(nameof(CategoryDto.Name),"test");
+            //result.Skip(0).Take(1).FirstOrDefault(x=>x.Id==1).ShouldNotBeNull();
+        }
+    }
+}
---
+++ TopicCleanArchitecture/TopicCleanArchitecture.Application.UnitTests/Mocks/MockCategoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopicCleanArchitecture.Application.Contracts.Persistence;
+using TopicCleanArchitecture.Domain;
+
+namespace TopicCleanArchitecture.Application.UnitTests.Mocks
+{
+    public class MockCategoryRepository
+    {
+        public static Mock<ICategoryRepository> GetCategoryMockCategoryRepository() {
+            var categories = new List<Category>
+            {
+                new Category { Id = 1,Name="Test Cat 1"},
+                new Category { Id = 2,Name="Test Cat 2"},
+                new Category { Id = 3,Name="Test Cat 3"}
+            };
+
+            var mockRepo= new Mock<ICategoryRepository>();
+
+            mockRepo.Setup(r=>r.GetAsync()).ReturnsAsync(categories);
+
+            mockRepo.Setup(r => r.CreateAsync(It.IsAny<Category>()))
+                .Returns((Category category) =>
+                {
+                    categories.Add(category);
+                    return Task.CompletedTask;
+                });
+
+            return mockRepo;
+        }
+    }
+}
