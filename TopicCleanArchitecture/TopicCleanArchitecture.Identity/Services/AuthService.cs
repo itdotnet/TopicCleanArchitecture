398bfb2eca5408b4ad6e70e693a4a7e6330c98be
omid <itmanager.abbasi@gmail.com>
2024-01-16 16:44:43 +0330

    commit corection of authservice and jwtKey

--- TopicCleanArchitecture/TopicCleanArchitecture.Identity/Services/AuthService.cs
+++ TopicCleanArchitecture/TopicCleanArchitecture.Identity/Services/AuthService.cs
@@ -18,16 +18,18 @@ namespace TopicCleanArchitecture.Identity.Services
     public class AuthService : IAuthService
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        private readonly JwtSettings _jwtSettings;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly JwtSettings _jwtSettings;
 
-        public AuthService(UserManager<ApplicationUser> userManager,JwtSettings jwtSettings,
+        public AuthService(UserManager<ApplicationUser> userManager,
+            IOptions<JwtSettings> jwtSettings,
             SignInManager<ApplicationUser> signInManager)
         {
-            this._userManager = userManager;
-            this._jwtSettings = jwtSettings;
-            this._signInManager = signInManager;
+            _userManager = userManager;
+            _jwtSettings = jwtSettings.Value;
+            _signInManager = signInManager;
         }
+
         public async Task<AuthResponse> Login(AuthRequest request)
         {
             var user = await _userManager.FindByEmailAsync(request.Email);
@@ -39,22 +41,24 @@ namespace TopicCleanArchitecture.Identity.Services
 
             var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
 
-            if (!result.Succeeded)
+            if (result.Succeeded == false)
             {
                 throw new BadRequestException($"Credentials for '{request.Email} aren't valid'.");
             }
 
             JwtSecurityToken jwtSecurityToken = await GenerateToken(user);
 
-            var response = new AuthResponse() { 
-                Id=user.Id,
-                Token=new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken),
-                Email=user.Email,
-                UserName=user.UserName
+            var response = new AuthResponse
+            {
+                Id = user.Id,
+                Token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken),
+                Email = user.Email,
+                UserName = user.UserName
             };
 
             return response;
         }
+
 
         public async Task<RegistrationResponse> Register(RegistrationRequest request)
         {
@@ -95,10 +99,10 @@ namespace TopicCleanArchitecture.Identity.Services
 
             var claims = new[]
             {
-                new Claim(JwtRegisteredClaimNames.Sub,user.UserName),
-                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
-                new Claim(JwtRegisteredClaimNames.Email,user.Email),
-                new Claim("uid",user.Id),
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("uid", user.Id)
             }
             .Union(userClaims)
             .Union(roleClaims);
@@ -115,5 +119,6 @@ namespace TopicCleanArchitecture.Identity.Services
                signingCredentials: signingCredentials);
             return jwtSecurityToken;
         }
+
     }
 }
